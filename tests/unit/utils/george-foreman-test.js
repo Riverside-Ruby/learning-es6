e1cffe7af9382a5232169122dd90cd4fb468f9d4
Jack Lucky <jack.lucky.iv@gmail.com>
2015-07-08 07:22:11 -0700

    Add test for George Foreman II

--- tests/unit/utils/george-foreman-test.js
+++ tests/unit/utils/george-foreman-test.js
@@ -4,7 +4,8 @@ import { module, test } from 'qunit';
 module('Unit | Utility | george foreman');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  var result = new GeorgeForeman();
-  assert.ok(result);
+test('it correctly names the second George Foreman', (assert) => {
+  var first = new GeorgeForeman();
+  var second = new GeorgeForeman();
+  assert.equal(second.name(), "George Foreman II");
 });
