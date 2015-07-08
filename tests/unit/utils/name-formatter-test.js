6eeea54f384c8a7d5a71d6b7882067c98aa1d546
Jack Lucky <jack.lucky.iv@gmail.com>
2015-07-08 06:50:56 -0700

    Refactor test using arrow function syntax

--- tests/unit/utils/name-formatter-test.js
+++ tests/unit/utils/name-formatter-test.js
@@ -4,7 +4,7 @@ import { module, test } from 'qunit';
 module('Unit | Utility | name formatter');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
+test('it works', (assert) => {
   var name = { first: "John", last: "Doe" };
   var formatter = new NameFormatter(name);
   assert.equal(formatter.fullName(), "John Doe");
