a63d81110024f34a11b0af25694a6667d9b167c1
Jack Lucky <jack.lucky.iv@gmail.com>
2015-07-08 07:10:18 -0700

    Add George Foreman class

---
+++ app/utils/george-foreman.js
@@ -0,0 +1,4 @@
+export default class GeorgeForeman {
+  constructor() {
+  }
+}
---
+++ tests/unit/utils/george-foreman-test.js
@@ -0,0 +1,10 @@
+import GeorgeForeman from '../../../utils/george-foreman';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | george foreman');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = new GeorgeForeman();
+  assert.ok(result);
+});
