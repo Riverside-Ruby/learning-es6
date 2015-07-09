5525d5f5d22faf3a65324de927de277cbb877e94
Jack Lucky <jack.lucky.iv@gmail.com>
2015-07-08 20:01:37 -0700

    Implement class/static method for George

--- app/utils/george-foreman.js
+++ app/utils/george-foreman.js
@@ -1,4 +1,28 @@
 export default class GeorgeForeman {
   constructor() {
+    this.generation = GeorgeForeman.addGeneration();
   }
+
+  name() {
+    return `George Foreman ${this.formatGeneration()}`;
+  }
+
+  formatGeneration() {
+    switch (this.generation) {
+      case 2:
+        return "II";
+      default:
+        return "";
+    }
+  }
+
+  static addGeneration() {
+    if(!this.generation && this.generation !== 1) {
+      this.generation = 1;
+    } else {
+      this.generation += 1;
+    }
+    return this.generation;
+  }
+
 }
